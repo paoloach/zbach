63a6a6b0a27b5bd9e0c20562935b5a076d7bc175
paolo achdjian <pachdjian@gmail.com>
2020-12-26 12:08:17 +0100

    OnOff cluster send report evert time his status change

--- clusters/ClusterOnOff.c
+++ clusters/ClusterOnOff.c
@@ -14,7 +14,12 @@
 
 extern uint8 connected;
 
-uint8  onOffValue = LIGHT_ON;
+static uint8  onOffValue = LIGHT_ON;
+static uint16 onTime=0;
+static uint8 lastEndpoint;
+static afAddrType_t * lastDstAddr;
+static uint8 * lastSegNum=NULL; 
+
 
 static void setIOStatus(void);
 
@@ -36,7 +41,9 @@ void onOffClusterReadAttribute(zclAttrRec_t * attribute) {
 		attribute->dataPtr = (void *)&onOffValue;
 		break;
         case ATTRID_ON_OFF_ON_TIME:
-          
+                attribute->dataType = ZCL_DATATYPE_UINT16;
+		attribute->dataPtr = (void *)&onTime;
+		break;
 	default:
 		attribute->status = ZCL_STATUS_UNSUPPORTED_ATTRIBUTE;
 	}
@@ -54,15 +61,24 @@ void onOffClusterWriteAttribute(ZclWriteAttribute_t * writeAttribute) {
 				setIOStatus();
 			}else
 				writeAttribute->status = ZCL_STATUS_INVALID_DATA_TYPE;
+                case ATTRID_ON_OFF_ON_TIME:
+                        if (writeAttribute->dataType == ZCL_DATATYPE_UINT16){
+				onTime = *(uint16 *)writeAttribute->dataPtr;
+			}else
+				writeAttribute->status = ZCL_STATUS_INVALID_DATA_TYPE;        
 		break;
 	}
 }
 
 void setIOStatus(void){
-	if ( onOffValue  == LIGHT_ON )
-	  P0_0=0;
-  	else
-	  P0_0=1;
+  if ( onOffValue  == LIGHT_ON )
+    P0_0=0;
+  else
+    P0_0=1;
+  
+  if (connected && lastSegNum != NULL){
+    onOffClusterSendReport(lastEndpoint, lastDstAddr, lastSegNum);
+  }
 }
 
 void setLightStatus(uint8 status){
@@ -95,6 +111,10 @@ ZStatus_t processOnOffClusterServerCommands(zclIncoming_t *pInMsg) {
 
 void onOffClusterSendReport(uint8 endpoint, afAddrType_t * dstAddr, uint8 * segNum) {
   if (connected){
+    lastEndpoint = endpoint;
+    lastDstAddr = dstAddr;
+    lastSegNum = segNum;
+    
     zclReportCmd_t * pReportCmd = osal_mem_alloc( sizeof(zclReportCmd_t) + sizeof(zclReport_t) );
     if ( pReportCmd != NULL ) {
       pReportCmd->numAttr = 1;
