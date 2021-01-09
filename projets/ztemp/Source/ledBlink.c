160a93d4d25d69e911217a48315c43b7a6c6f7fc
paolo achdjian <pachdjian@gmail.com>
2021-01-09 09:37:23 +0100

    Use pin configuration for blinking led

--- projets/ztemp/Source/ledBlink.c
+++ projets/ztemp/Source/ledBlink.c
@@ -1,34 +1,9 @@
 #include "onboard.h"
 #include "OSAL_Timers.h"
-
+#include "regs.h"
 #include "ClusterOSALEvents.h"
 
 #include "ledBlink.h"
-
-
-
-__sfr __no_init volatile struct  {
-	unsigned char DIR0_0: 1;
-	unsigned char DIR0_1: 1;
-	unsigned char DIR0_2: 1;
-	unsigned char DIR0_3: 1;
-	unsigned char DIR0_4: 1;
-	unsigned char DIR0_5: 1;
-	unsigned char DIR0_6: 1;
-	unsigned char DIR0_7: 1;
-} @ 0xFD;
-
-__sfr __no_init volatile struct  {
-	unsigned char P0SEL_0: 1;
-	unsigned char P0SEL_1: 1;
-	unsigned char P0SEL_2: 1;
-	unsigned char P0SEL_3: 1;
-	unsigned char P0SEL_4: 1;
-	unsigned char P0SEL_5: 1;
-	unsigned char P0SEL_6: 1;
-	unsigned char P0SEL_7: 1;
-} @ 0xF3;
-
 
 #define FAST_BLINK_TIME_ON 100	 
 #define FAST_BLINK_TIME_OFF_LONG 2000	 
@@ -44,22 +19,23 @@ void setBlinkCounter(byte blinkCount) {
 }
 
 void blinkLedInit(void) {
-	DIR0_1 = 1;
- 	P0SEL_1 = 0;
- 	P0_1 = 0;
-        stop=0;
+  DIR(LED_BLINK_PORT, LED_BLINK_PIN)=1;
+  FUNCTION_SEL(LED_BLINK_PORT,   LED_BLINK_PIN)=0;
+  PORT(LED_BLINK_PORT, LED_BLINK_PIN)=0;
+
+  stop=0;
 }
 
 
 void blinkLedstart(byte taskid){
-	osal_start_timerEx( taskid, FAST_BLINK, FAST_BLINK_TIME_ON );
-	P0_1 = 1;
-        stop=0;
+  osal_start_timerEx( taskid, FAST_BLINK, FAST_BLINK_TIME_ON );
+  PORT(LED_BLINK_PORT, LED_BLINK_PIN) = 1;
+  stop=0;
 }
 
 void blinkLedEnd(byte taskid){
 	osal_stop_timerEx( taskid, FAST_BLINK );
-	P0_1 = 0;
+	PORT(LED_BLINK_PORT, LED_BLINK_PIN) = 0;
         stop=1;
 }
 
@@ -67,8 +43,8 @@ void blinkLedEnd(byte taskid){
 void blinkLedAction(byte taskid){
   if (stop)
     return;
-  if (P0_1){
-          P0_1 = 0;
+  if (PORT(LED_BLINK_PORT, LED_BLINK_PIN)){
+          PORT(LED_BLINK_PORT, LED_BLINK_PIN) = 0;
           if (counter > 0){
                   osal_start_timerEx( taskid, FAST_BLINK, FAST_BLINK_TIME_OFF_SHORT );
                   counter--;
@@ -77,7 +53,7 @@ void blinkLedAction(byte taskid){
                   counter=blink;
           }
   }else{
-          P0_1 = 1;
+          PORT(LED_BLINK_PORT, LED_BLINK_PIN) = 1;
           osal_start_timerEx( taskid, FAST_BLINK, FAST_BLINK_TIME_ON );
   }
 }
